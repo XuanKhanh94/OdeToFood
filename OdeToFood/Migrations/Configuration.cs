edc9025682b57f14f09d321475b1e3e8a72aab75
XuanKhanh94 <xuankhanh379@gmail.com>
2023-08-16 21:27:18 +0700

    S22 Database Migrations

---
+++ OdeToFood/Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+namespace OdeToFood.Migrations
+{
+    using OdeToFood.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OdeToFood.Models.OdeToFoodDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "OdeToFood.Models.OdeToFoodDb";
+        }
+
+        protected override void Seed(OdeToFood.Models.OdeToFoodDb context)
+        {
+            context.Restaurants.AddOrUpdate(r => r.Name,
+                new Restaurant { Name = "Khanh", City = "HCM", Country = "VN" },
+                new Restaurant { Name = "Nam", City = "DN", Country = "VN" },
+                new Restaurant
+                {
+                    Name = "Khai",
+                    City = "Hai Phong",
+                    Country = "VN",
+                    Reviews =
+                    new List<RestaurantReview>
+                    {
+                        new RestaurantReview{Rating=9,Body="Great food!",ReviewerName="Scott"}
+                    }
+                });
+        }
+    }
+}
