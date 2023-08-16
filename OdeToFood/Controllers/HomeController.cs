8ee844b04c66d811098fdb07767a75d591f8a2d3
XuanKhanh94 <xuankhanh379@gmail.com>
2023-08-16 22:18:21 +0700

    S23 Using LINQ

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -12,9 +12,29 @@ namespace OdeToFood.Controllers
         OdeToFoodDb _db = new OdeToFoodDb();
         public ActionResult Index()
         {
-            var model = _db.Restaurants.ToList();
-
-
+            //var model =
+            //    from r in _db.Restaurants
+            //    orderby r.Reviews.Average(review => review.Rating) descending
+            //    select new RestaurantListViewModel
+            //    {
+            //        Id = r.Id,
+            //        Name = r.Name,
+            //        City = r.City,
+            //        Country = r.Country,
+            //        CountOfReviews = r.Reviews.Count()
+            //    };
+            var model =
+                _db.Restaurants
+                .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                .Take(10)
+                .Select(r => new RestaurantListViewModel
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    City = r.City,
+                    Country = r.Country,
+                    CountOfReviews = r.Reviews.Count()
+                });
             return View(model);
         }
 
