7f30c8a895d4d2d2240b212436adcd62786a8d18
XuanKhanh94 <xuankhanh379@gmail.com>
2023-08-21 23:52:38 +0700

    S27 Listing Reviews

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -1,136 +1,31 @@
-﻿//using OdeToFood.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace OdeToFood.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
-//        [ChildActionOnly]
-//        public ActionResult BestReview()
-//        {
-//            var bestReview = from r in _reviews
-//                             orderby r.Rating descending
-//                             select r;
-//            return PartialView("_Review", bestReview.First());
-//        }
-//        // GET: Reviews
-//        public ActionResult Index()
-//        {
-//            var model =
-//                from r in _reviews
-//                orderby r.Country
-//                select r;
-//            return View(model);
-//        }
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+            {
+                return View(restaurant);
+            }
+            return HttpNotFound();
+        }
 
-//        // GET: Reviews/Details/5
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
 
-//        // GET: Reviews/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
+        }
 
-//        // POST: Reviews/Create
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        // GET: Reviews/Edit/5
-//        public ActionResult Edit(int id)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            return View(review);
-//        }
-
-//        // POST: Reviews/Edit/5
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            var review = _reviews.Single(r => r.Id == id);
-//            if (TryUpdateModel(review))
-//            {
-//                //...
-//                return RedirectToAction("Index");
-//            }
-//            return View(review);
-//        }
-
-//        // GET: Reviews/Delete/5
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        // POST: Reviews/Delete/5
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-//        static List<RestaurantReview> _reviews = new List<RestaurantReview>
-//        {
-//            new RestaurantReview
-//            {
-//                Id=1,
-//                Name="Cinnamon Club",
-//                City="<script>alert('sad');</script>",
-//                Country="UK",
-//                Rating=10,
-//            },
-//              new RestaurantReview
-//            {
-//                Id=2,
-//                Name="Dinnamon Club",
-//                City="Paris",
-//                Country="FR",
-//                Rating=10,
-//            },
-//                new RestaurantReview
-//            {
-//                Id=3,
-//                Name="Cinn Club",
-//                City="Tokyo",
-//                Country="JP",
-//                Rating=7,
-//            },
-//                new RestaurantReview
-//            {
-//                Id=4,
-//                Name="Kin Club",
-//                City="HaNoi",
-//                Country="VN",
-//                Rating=9,
-//            },
-//         };
-
-//    }
-//}
+    }
+}
