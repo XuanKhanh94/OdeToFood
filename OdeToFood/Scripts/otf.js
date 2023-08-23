637d246d4d96d309f8c2c8be255df9d0bbc95a4e
XuanKhanh94 <xuankhanh379@gmail.com>
2023-08-23 15:02:59 +0700

    S37 An Async Search

---
+++ OdeToFood/Scripts/otf.js
@@ -0,0 +1,22 @@
+﻿
+
+
+$(function () {
+
+    var ajaxFormSubmit = function () {
+        var $form = $(this);
+        var option = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+        $.ajax(option).done(function (data) {
+            var $target = $($form.attr("data-otf-target"));
+            $target.replaceWith(data);
+        });
+        return false;
+    };
+
+
+    $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+});
